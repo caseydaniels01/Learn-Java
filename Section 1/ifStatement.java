9f2643cea55d27688f830015f03b0d568e87ec58
caseydaniels01 <caseydaniels01@gmail.com>
2024-05-06 21:57:05 -0500

    ifStatement

---
+++ Section 1/ifStatement.java
@@ -0,0 +1,15 @@
+public class ifStatement {
+
+ public static void main(String[] args) {
+    
+    boolean isAlien = false;
+    if (!isAlien) {
+        System.out.println("It is not an alien");
+    }
+    
+    else {
+        System.out.println("It is an alien");
+    }
+ }   
+    
+}
